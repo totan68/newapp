ba43afbe1885cbdf2b53858b6849c20316b528c8
totan68 <164209696+totan68@users.noreply.github.com>
2024-08-26 16:58:00 +0530

    Initial Commit

---
+++ newapp/newappApp.swift
@@ -0,0 +1,17 @@
+//
+//  newappApp.swift
+//  newapp
+//
+//  Created by Totan Hazra on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct newappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
